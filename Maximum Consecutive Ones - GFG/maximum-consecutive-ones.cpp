83d15ba92d74c2c95ff51fb655bc6971178178fc
demondragon07 <127862678+demondragon07@users.noreply.github.com>
2023-03-14 23:02:57 +0530

    Added solution - LeetHub

---
+++ Maximum Consecutive Ones - GFG/maximum-consecutive-ones.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int longestOnes(int n, vector<int>& nums, int k) {
+        int i=0,j=0;
+        int ans=0;
+        int count=0;
+        
+        while(j<nums.size()){
+            if(nums[j]==0){
+                count++;
+            }
+            while(count>k){
+                if(nums[i]==0){
+                    count--;
+                }
+                i++;
+            }
+            
+            ans=max(ans,j-i+1);
+            j++;
+            
+        }
+    
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> nums;
+        for (int i = 0; i < n; ++i) {
+            int x;
+            cin >> x;
+            nums.push_back(x);
+        }
+
+        int k;
+        cin >> k;
+
+        Solution obj;
+        cout << obj.longestOnes(n, nums, k) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
